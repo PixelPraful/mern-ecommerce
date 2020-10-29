1faea86f13b9f066ce77235dea0a584ce1d62532
Praful <praful@taglr.com>
2020-10-29 10:32:08 +0530

    Backend integration with db

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -44,6 +44,7 @@ module.exports = {
     'jsx-a11y/label-has-for': [0],
     'function-paren-newline': [0],
     'jsx-a11y/no-static-element-interactions': [0],
-    'indent': ['error', 2]
+    'indent': ['error', 2],
+    semi: ['error', 'always']
   }
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
